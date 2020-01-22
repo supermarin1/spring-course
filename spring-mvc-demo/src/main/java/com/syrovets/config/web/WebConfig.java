3237a69a89ec1523e891331617c9a079ce38bee3
msyrovets <msyrovets@s-pro.io>
2020-01-22 14:41:56 +0200

    -- custom error messages

--- spring-mvc-demo/src/main/java/com/syrovets/config/web/WebConfig.java
+++ spring-mvc-demo/src/main/java/com/syrovets/config/web/WebConfig.java
@@ -1,8 +1,11 @@
 package com.syrovets.config.web;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.MediaType;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -63,6 +66,23 @@ public class WebConfig implements WebMvcConfigurer {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 
     @Override
--- spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
@@ -5,5 +5,9 @@
 
 <body>
 The customer is confirmed: ${customer.firstName} ${customer.lastName}
+<br><br>
+Free passes: ${customer.freePasses}
+<br><br>
+Postal code: ${customer.postalCode}
 </body>
 </html>
